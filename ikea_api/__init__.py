5bc0e3b57576c52d1e18dc39f2c0afe933b6baf9
Lev <levwint@gmail.com>
2021-03-31 16:44:08 +0300

    Add modules to __init__.py

--- ikea_api/__init__.py
+++ ikea_api/__init__.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
+
+from .auth import Auth
+from .cart import Cart
+from .constants import Constants
+from .item import fetch_items_specs
+from .profile import Profile
+
 __version__ = '0.0.1'
