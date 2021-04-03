25219cc90fde0a048b4256a2ff55e4133d3d2c74
Lev <levwint@gmail.com>
2021-04-03 13:50:04 +0300

    Separate authorization into functions

--- ikea_api/auth.py
+++ ikea_api/auth.py
@@ -1,42 +1,62 @@
 import string
-import re
 import json
 from random import SystemRandom
 from base64 import urlsafe_b64encode, b64decode
 from hashlib import sha256
+from urllib.parse import urlparse, parse_qs
 
-from urllib.parse import urlencode
-import requests
+from requests import Session, post
 from bs4 import BeautifulSoup
 
 from .constants import Constants
+from .utils import check_response
 
-class Auth():
-    def __init__(self):
-        self.base_headers = {
+
+def get_guest_token():
+    """Token expires in 30 days"""
+    url = 'https://api.ingka.ikea.com/guest/token'
+    headers = {
+        'Accept-Encoding': Constants.ACCEPT_ENCODING,
+        'Accept-Language': 'ru',
+        'Origin': 'https://www.ikea.com',
+        'Referer': 'https://www.ikea.com/',
+        'Connection': 'keep-alive',
+        'User-Agent': Constants.USER_AGENT,
+        'X-Client-Id': 'e026b58d-dd69-425f-a67f-1e9a5087b87b',
+        'X-Client-Secret': 'cP0vA4hJ4gD8kO3vX3fP2nE6xT7pT3oH0gC5gX6yB4cY7oR5mB'
+    }
+    payload = {'retailUnit': 'ru'}
+    response = post(url, headers=headers, json=payload)
+    check_response(response)
+    token = response.json()['access_token']
+    return token
+
+
+class Auth:
+    """
+    OAuth2 authorization
+    Token expires in 24 hours
+    """
+
+    def __init__(self, username, password):
+        self.session = Session()
+        self.session.headers.update({
+            'Accept-Language': 'en-us',
             'Accept-Encoding': Constants.ACCEPT_ENCODING,
+            'Origin': 'https://www.ikea.com',
             'Connection': 'keep-alive',
             'User-Agent': Constants.USER_AGENT
-        }
-
-    def get_guest_token(self):
-        url = 'https://api.ingka.ikea.com/guest/token'
-        headers = self.base_headers
-        headers.update({
-            'Accept-Language': 'ru',
-            'Host': 'api.ingka.ikea.com',
-            'X-Client-Id': 'e026b58d-dd69-425f-a67f-1e9a5087b87b',
-            'X-Client-Secret': 'cP0vA4hJ4gD8kO3vX3fP2nE6xT7pT3oH0gC5gX6yB4cY7oR5mB',
-            'Origin': 'https://www.ikea.com',
-            'Referer': 'https://www.ikea.com/'
         })
-        payload = {'retailUnit': 'ru'}
-        response = requests.post(url, headers=headers,
-                                 json=payload)
-        token = response.json()['access_token']
-        return token
+        auth0_authorize = self._auth0_authorize()
+        usernamepassword_login = self._usernamepassword_login(
+            username, password, auth0_authorize['session_config'], auth0_authorize['url'])
+        login_callback = self._login_callback(
+            usernamepassword_login['wctx'], usernamepassword_login['wresult'], usernamepassword_login['url'])
+        oauth_token = self._oauth_token(
+            login_callback['code'], login_callback['url'])
+        self.token = oauth_token
 
-    def create_s256_code_challenge(self, code_verifier):
+    def _create_s256_code_challenge(self, code_verifier):
         """
         https://github.com/lepture/authlib
         Create S256 code_challenge with the given code_verifier.
@@ -45,44 +65,52 @@ class Auth():
             bytes(code_verifier.encode('ascii', 'strict'))).digest()
         return urlsafe_b64encode(data).rstrip(b'=').decode('utf-8', 'strict')
 
-    def generate_token(self):
+    def _generate_token(self):
         """https://github.com/lepture/authlib"""
         rand = SystemRandom()
         return ''.join(rand.choice(string.ascii_letters + string.digits) for _ in range(48))
 
-    def get_authorized_token(self, username, password):
-        """OAuth2 authorization"""
-        session = requests.Session()
-        headers = self.base_headers
-        headers['Accept-Language'] = 'en-us'
-
-        # 1. /autorize  Autorize; redirects to /login with useful info
-        code_verifier = self.generate_token()
-        authorize_endpoint = 'https://ru.accounts.ikea.com/authorize?' + urlencode({
+    def _auth0_authorize(self):
+        """
+        1. /autorize
+        Get Auth0 config
+        """
+        self.code_verifier = self._generate_token()
+        endpoint = 'https://ru.accounts.ikea.com/authorize'
+        params = {
             'client_id': '72m2pdyUAg9uLiRSl4c4b0b2tkVivhZl',
             'redirect_uri': 'https://www.ikea.com/ru/ru/profile/login/',
             'response_type': 'code',
             'ui_locales': 'ru-RU',
-            'code_chalenge': self.create_s256_code_challenge(code_verifier),
+            'code_chalenge': self._create_s256_code_challenge(self.code_verifier),
             'code_chalenge_method': 'S256',
             'scope': 'openid profile email',
             'audience': 'https://retail.api.ikea.com',
             'registration': '{"bveventid":null}',
             'consumer': 'OWF',
-            'state': self.generate_token(),
+            'state': self._generate_token(),
             'auth0Client': 'eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOS4xNC4zIn0='
-        })
-        authorize_headers = headers
-        authorize_headers['Accept']: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8.'
-        authorize_response = session.get(
-            authorize_endpoint, headers=authorize_headers)
-        encoded_session_config = BeautifulSoup(authorize_response.text, 'html.parser').find(
-            'script', id='a0-config').get('data-config')
-        session_config = json.loads(b64decode(encoded_session_config))
+        }
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8.',
+            'Referer': 'https://www.ikea.com/'
+        }
+        response = self.session.get(
+            endpoint, params=params, headers=headers)
+        check_response(response)
 
-        # 2. /usernamepassword/login    Log in system
-        usrpwd_login_endpoint = 'https://ru.accounts.ikea.com/usernamepassword/login'
-        usrpwd_login_payload = {
+        encoded_config = BeautifulSoup(response.text, 'html.parser').find(
+            'script', id='a0-config').get('data-config')
+        session_config = json.loads(b64decode(encoded_config))
+        return {'session_config': session_config, 'url': response.url}
+
+    def _usernamepassword_login(self, usr, pwd, session_config, authorize_final_url):
+        """
+        2. /usernamepassword/login
+        Log in and get wctx and wresult params
+        """
+        endpoint = 'https://ru.accounts.ikea.com/usernamepassword/login'
+        payload = {
             'client_id': session_config['clientID'],
             'redirect_uri': session_config['callbackURL'],
             'tenant': session_config['auth0Tenant'],
@@ -92,59 +120,66 @@ class Auth():
             '_csrf': session_config['extraParams']['_csrf'],
             'state': session_config['extraParams']['state'],
             '_intstate': session_config['extraParams']['_intstate'],
-            'username': username,
-            'password': password,
+            'username': usr,
+            'password': pwd,
             'connection': 'Username-Password-Authentication'
         }
-        usrpwd_login_headers = headers
-        usrpwd_login_headers.update({
-            'Referer': authorize_response.url,
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8.',
+            'Referer': authorize_final_url,
             'Auth0-Client': session_config['extraParams']['auth0Client'],
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8.'
-        })
-        usrpwd_login_result = session.post(
-            usrpwd_login_endpoint, headers=usrpwd_login_headers, json=usrpwd_login_payload)
-        if not usrpwd_login_result.ok:
-            raise Exception(usrpwd_login_result.json())
+        }
+        response = self.session.post(
+            endpoint, headers=headers, json=payload)
+        check_response(response)
 
-        # 3. /login/callback    Get code parameter from callback
-        soup = BeautifulSoup(usrpwd_login_result.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 'html.parser')
         wctx = soup.find('input', {'name': 'wctx'}).get('value')
         wresult = soup.find('input', {'name': 'wresult'}).get('value')
+        return {'wctx': wctx, 'wresult': wresult, 'url': response.url}
 
-        login_callback_endpoint = 'https://ru.accounts.ikea.com/login/callback'
-        login_callback_headers = headers
-        login_callback_headers.update({
-            'Content-Type': 'application/x-www-form-urlencoded',
+    def _login_callback(self, wctx, wresult, usernamepassword_login_final_url):
+        """
+        3. /login/callback
+        Get code parameter from callback
+        """
+        endpoint = 'https://ru.accounts.ikea.com/login/callback'
+        headers = {
             'Origin': 'https://ru.accounts.ikea.com',
-            'Referer': usrpwd_login_result.url
-        })
-        login_callback_payload = 'wa=wsignin1.0&wresult=%s&wctx=%s' % (
-            wresult, wctx)
-        login_callback_response = session.post(
-            login_callback_endpoint, headers=login_callback_headers, data=login_callback_payload)
+            'Referer': usernamepassword_login_final_url
+        }
+        payload = {
+            'wa': 'wsignin1.0',
+            'wresult': wresult,
+            'wctx': wctx
+        }
+        response = self.session.post(
+            endpoint, headers=headers, data=payload)
+        check_response(response)
 
-        # 4. /oauth/token   Get access token
-        oauth_token_enpoint = 'https://ru.accounts.ikea.com/oauth/token'
-        oauth_token_headers = headers
-        oauth_token_headers.update({
-            'Content-Type': 'application/json',
-            'Origin': 'https://www.ikea.com',
-            'Accept-Encoding': 'gzip, deflate',  # TODO: Need to add 'br' encoding format
+        code = parse_qs(urlparse(response.url).query)['code'][0]
+        return {'code': code, 'url': response.url}
+
+    def _oauth_token(self, callback_code, callback_final_url):
+        """
+        4. /oauth/token
+        Get access token
+        """
+        endpoint = 'https://ru.accounts.ikea.com/oauth/token'
+        headers = {
             'Accept': '*/*',
-            'Referer': login_callback_response.url,
-        })
-        oauth_token_payload = {
+            'Referer': callback_final_url
+        }
+        payload = {
             'grant_type': 'authorization_code',
             'client_id': '72m2pdyUAg9uLiRSl4c4b0b2tkVivhZl',
-            'code_verifier': code_verifier,
-            'code': re.search(r'code=([^"&]+)', login_callback_response.url)[1],
+            'code_verifier': self.code_verifier,
+            'code': callback_code,
             'redirect_uri': 'https://www.ikea.com/ru/ru/profile/login/',
             'scope': 'openid profile email'
         }
-        oauth_token_response = session.post(
-            oauth_token_enpoint,
-            headers=oauth_token_headers,
-            json=oauth_token_payload)
-        token = oauth_token_response.json()['access_token']
+        response = self.session.post(endpoint, headers=headers, json=payload)
+        check_response(response)
+
+        token = response.json()['access_token']
         return token
---
+++ ikea_api/utils.py
@@ -0,0 +1,29 @@
+from .errors import TokenExpiredError, NotAuthenticatedError
+
+
+def check_response(response):
+    if not response.ok:
+        raise Exception(response.status_code, response.content)
+
+
+def call_api(self, data):
+    """Call one of IKEA's GraphQL API"""
+    response = self.session.post(self.endpoint, json=data)
+    response_json = response.json()
+    if 'errors' in response_json:
+        if 'extensions' in response_json['errors'][0]:
+            if 'errorCode' in response_json['errors'][0]['extensions']:
+                if response_json['errors'][0]['extensions']['errorCode'] == 401:
+                    raise NotAuthenticatedError
+        else:
+            if 'message' in response_json['errors'][0]:
+                raise Exception(response_json['errors'][0]['message'])
+            else:
+                raise Exception(response_json['errors'][0])
+    if response.status_code == 401 and 'error' in response_json:
+        if response_json['error'] == 'Token has expired':
+            raise TokenExpiredError
+        else:
+            raise NotAuthenticatedError(response_json['error'])
+    check_response(response)
+    return response_json
