b7100129435a6e5dc67d678a00d46dcd03d3d153
Lev <levwint@gmail.com>
2021-04-17 16:52:42 +0300

    Update setup.py

--- ikea_api/__init__.py
+++ ikea_api/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from .auth import Auth, get_guest_token
 from .endpoints import Cart, Profile, OrderCapture
 from .misc.item import fetch_items_specs
--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
 	author='vrslev',
 	author_email='5225148+vrslev@users.noreply.github.com',
     license='MIT',
-	packages=['ikea_api'],
+	packages=['ikea_api', 'ikea_api.endpoints', 'ikea_api.misc'],
 	install_requires=install_requires
 )
