5201db27a673453add034d49031c53372dc7d965
Lev <levwint@gmail.com>
2021-04-17 16:13:52 +0300

    Delete example

--- examples/get_delivery_services.py
+++
@@ -1,8 +0,0 @@
-from ikea_api import get_guest_token, Cart, OrderCapture
-
-token = get_guest_token()
-cart = Cart(token)
-cart.add_items({'30457903': 1})
-order_capture = OrderCapture(token, 'your_zip_code')
-deliveries = order_capture.get_delivery_services()
-print(deliveries)
