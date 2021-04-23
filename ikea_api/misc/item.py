3ffecb3ba20c3cd05ec104055ae3b0eef67fa462
Lev <levwint@gmail.com>
2021-04-23 21:18:26 +0300

    Improve item fetching

--- ikea_api/misc/item.py
+++ ikea_api/misc/item.py
@@ -1,8 +1,17 @@
-import json
-import requests
+from requests import Session
+from ..utils import parse_item_code
+from ..api import USER_AGENT
 
 
-def _build_url(items):
+class ItemFetchError(Exception):
+    pass
+
+
+class WrongItemCodeError(Exception):
+    pass
+
+
+def _build_url(items: dict):
     endpoint = 'https://iows.ikea.com/retail/iows/ru/ru/catalog/items/'
     templated_list = []
     for item in items:
@@ -10,97 +19,86 @@ def _build_url(items):
     return endpoint + ';'.join(templated_list)
 
 
-def _return_response(response, is_list):
-    if is_list:
-        return response['RetailItemCommList']['RetailItemComm']
-    else:
-        return [response['RetailItemComm']]
-
-
-def _fetch_items_specs(input_items):
-    headers = {
+def _fetch_items_specs(input_items: list):
+    session = Session()
+    session.headers.update({
         'Accept': 'application/vnd.ikea.iows+json;version=2.0',
         'Origin': 'https://www.ikea.com',
         'Referer': 'https://www.ikea.com/ru/ru/shoppinglist/',
         'Cache-Control': 'no-cache, no-store',
         'Host': 'iows.ikea.com',
         'Accept-Language': 'ru',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
+        'User-Agent': USER_AGENT,
         'Accept-Encoding': 'gzip, deflate, br',
         'Connection': 'keep-alive',
         'consumer': 'MAMMUT#ShoppingList',
         'contract': '37249'
-    }
-    session = requests.Session()
+    })
 
-    if type(input_items) is list:
-        is_list = False if len(input_items) == 1 else True
-    else:
-        is_list = False
-        input_items = [input_items]
-
+    if len(input_items) == 0:
+        return
     items = {}
     for item in input_items:
         items[item] = 'ART'
 
-    # 1. Get data, catch errors
-    url = _build_url(items)
-    response = session.get(url, headers=headers)
-    if response.status_code != 404:
-        response_res = response.json()
-    else:
-        # If 404 and only one item is being fetched
-        # then it's probably SPR
-        if len(items) == 1:
+    for i in range(3):
+        url = _build_url(items)
+        response = session.get(url)
+        if i == 0 and len(items) == 1 and not response.ok:
             items[item] = 'SPR'
             url = _build_url(items)
-            response = session.get(url, headers=headers)
-            if response.status_code == 404:
-                raise ValueError('Wrong item code')
+            response = session.get(url)
+            if not response.ok:
+                raise WrongItemCodeError(input_items[0])
+
+        r_json = response.json()
+
+        if 'RetailItemCommList' in r_json:
+            if 'RetailItemComm' in r_json['RetailItemCommList']:
+                return r_json['RetailItemCommList']['RetailItemComm']
             else:
-                response_res = response.json()
+                raise ItemFetchError(r_json)
 
-    try:
-        return _return_response(response_res, is_list)
-    except KeyError:
-        # 2. If error than some items are SPRs. Finding out which ones
-        errors = response_res['ErrorList']['Error']
-        if type(errors) is not list:
-            errors = [errors]
-        for error in errors:
-            item_code = str(error['ErrorAttributeList']
-                            ['ErrorAttribute'][0]['Value']['$'])
-            item_type = error['ErrorAttributeList']['ErrorAttribute'][1]['Value']['$']
-            items[item_code] = 'SPR' if item_type == 'ART' else 'ART'
-        repaired_url = _build_url(items)
-        repaired_response = session.get(repaired_url, headers=headers)
+        elif 'RetailItemComm' in r_json:
+            return [r_json['RetailItemComm']]
 
-        repaired_response_res = repaired_response.json()
-
-        try:
-            return _return_response(repaired_response_res, is_list)
-        except KeyError:
-            # 3. If item is not ART nor SPR it means there's no such item. Deleting such items
-            errors = repaired_response_res['ErrorList']['Error']
-            if not type(errors) is list:
+        elif 'ErrorList' in r_json:
+            errors = r_json['ErrorList']['Error']
+            if not isinstance(errors, list):
                 errors = [errors]
+
             for err in errors:
-                item_code = str(err['ErrorAttributeList']
-                                ['ErrorAttribute'][0]['Value']['$'])
-                item_type = err['ErrorAttributeList']['ErrorAttribute'][1]['Value']['$']
-                items.pop(item_code)
-            if len(items) == 1:
-                is_list = False
-            final_url = _build_url(items)
-            final_response = session.get(final_url, headers=headers)
-            final_response_res = final_response.json()
-            return _return_response(final_response_res, is_list)
+                for test in [
+                    'ErrorCode' in err,
+                    err['ErrorCode']['$'] == 1101,
+                    'ErrorMessage' in err,
+                    'ErrorAttributeList' in err,
+                    'ErrorAttribute' in err['ErrorAttributeList']
+                ]:
+                    if not test:
+                        raise ItemFetchError(err, test)
+
+                attrs = {}
+                for attr in err['ErrorAttributeList']['ErrorAttribute']:
+                    attrs[attr['Name']['$']] = attr['Value']['$']
+                item_code = str(attrs['ITEM_NO'])
+                item_type = attrs['ITEM_TYPE']
+
+                if i == 0:
+                    items[item_code] = 'SPR' if item_type == 'ART' else 'ART'
+                elif i == 1:
+                    items.pop(item_code)
 
 
 def fetch_items_specs(input_items):
-    if type(input_items) is str:
+    if isinstance(input_items, str):
         input_items = [input_items]
+    elif not isinstance(input_items, list):
+        raise TypeError('String or list required')
+
     input_items = list(set(input_items))
+    input_items = parse_item_code(input_items)
+
     chunks = [input_items[x:x+90] for x in range(0, len(input_items), 90)]
     responses = []
     for chunk in chunks:
