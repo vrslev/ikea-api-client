234139083e66323c770bef189c846d51df9a620a
Lev <levwint@gmail.com>
2021-05-10 12:22:06 +0300

    fix setup

--- ikea_api/__init__.py
+++ ikea_api/__init__.py
@@ -1,5 +1,3 @@
 from .auth import Auth, get_guest_token
 from .endpoints import Cart, Purchases, OrderCapture
 from .misc.item import fetch_items_specs
-
-__version__ = '0.0.4'
--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@ from setuptools import setup
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
-from ikea_api import __version__ as version
-
 setup(
 	name='ikea_api',
-	version=version,
+	version='0.0.4',
 	description='IKEA Api Client',
     url='https://github.com/vrslev/ikea-api-client',
 	author='vrslev',
