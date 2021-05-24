147d165e03ed892d632416b1dfd517477a3dc836
Lev <levwint@gmail.com>
2021-05-24 15:22:55 +0300

    Fix setup, change version

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
 setup(
 	name='ikea_api',
-	version='0.0.4',
+	version='0.0.5',
 	description='IKEA Api Client',
     url='https://github.com/vrslev/ikea-api-client',
 	author='vrslev',
 	author_email='5225148+vrslev@users.noreply.github.com',
     license='MIT',
-	packages=['ikea_api', 'ikea_api.endpoints', 'ikea_api.misc'],
+	packages=find_packages(),
 	install_requires=install_requires
 )
