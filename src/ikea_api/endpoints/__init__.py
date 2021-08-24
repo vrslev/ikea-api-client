2555866921d707ae0d2cac747c83131094104eb6
Lev <mail@vrslev.com>
2021-08-24 14:42:51 +0300

    fix(Item): Use `staticmethod` for various item fetch types

--- src/ikea_api/endpoints/__init__.py
+++ src/ikea_api/endpoints/__init__.py
@@ -1,18 +1,25 @@
+from __future__ import annotations
+
 from .cart import Cart
 from .item.item_ingka import fetch as _fetch_items_specs_ingka
 from .item.item_iows import fetch as _fetch_items_specs_iows
-from .item.item_pip import fetch as _fetch_item_specs_pip
+from .item.item_pip import fetch as _fetch_items_specs_pip
 from .order_capture import OrderCapture
 from .purchases import Purchases
 
 
-class _mock_fetch_items_specs:
-    def __init__(self) -> None:
-        self.ingka = _fetch_items_specs_ingka
-        self.iows = _fetch_items_specs_iows
-        self.pip = _fetch_item_specs_pip
+class fetch_items_specs:
+    @staticmethod
+    def ingka(items: str | list[str]):
+        return _fetch_items_specs_ingka(items)
 
+    @staticmethod
+    def iows(items: str | list[str]):
+        return _fetch_items_specs_iows(items)
 
-fetch_items_specs = _mock_fetch_items_specs()
+    @staticmethod
+    def pip(items: dict[str, bool]):
+        return _fetch_items_specs_pip(items)
+
 
 __all__ = ["Cart", "OrderCapture", "Purchases", "fetch_items_specs"]
