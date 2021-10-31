7a3088eed9b486009cd9c7835b41174a47ec523e
Lev <mail@vrslev.com>
2021-10-31 22:29:58 +0300

    test: Complete __init__.py coverage

--- tests/test_init.py
+++ tests/test_init.py
@@ -5,6 +5,9 @@ import pytest
 import ikea_api
 from ikea_api import IkeaApi
 from ikea_api._constants import Constants
+from ikea_api._endpoints.cart import Cart
+from ikea_api._endpoints.purchases import Purchases
+from ikea_api.types import SearchType
 
 
 @pytest.mark.parametrize(
@@ -33,7 +36,7 @@ def test_core_init_token(token: str | None):
 
 @pytest.fixture
 def core():
-    return IkeaApi()
+    return IkeaApi(token="test")  # nosec
 
 
 def test_core_login_not_guest(monkeypatch: pytest.MonkeyPatch, core: IkeaApi):
@@ -68,3 +71,53 @@ def test_core_cart(core: IkeaApi):
     old_cart = core.cart
     assert hasattr(core, "_cart")
     assert core.cart == old_cart
+    assert type(core.cart) == Cart
+    assert core.cart.token == core.token
+
+
+def test_core_order_capture(monkeypatch: pytest.MonkeyPatch, core: IkeaApi):
+    exp_zip_code = "101000"
+    exp_state_code = "10100"
+    called = False
+
+    class MockOrderCapture:
+        def __init__(self, token: str, zip_code: str, state_code: str | None):
+            assert token == core.token
+            assert zip_code == exp_zip_code
+            assert state_code == exp_state_code
+
+        def __call__(self):
+            nonlocal called
+            called = True
+
+    monkeypatch.setattr(ikea_api, "OrderCapture", MockOrderCapture)
+    core.order_capture(exp_zip_code, exp_state_code)
+    assert called
+
+
+def test_core_purchases(core: IkeaApi):
+    old_purchases = core.purchases
+    assert hasattr(core, "_purchases")
+    assert core.purchases == old_purchases
+    assert type(core.purchases) == Purchases
+    assert core.purchases.token == core.token
+
+
+def test_core_search(monkeypatch: pytest.MonkeyPatch, core: IkeaApi):
+    exp_query = "Billy"
+    exp_limit = 10
+    exp_types: list[SearchType] = ["PRODUCT", "CONTENT"]
+    called = False
+
+    class MockSearch:
+        def __call__(self, query: str, limit: int, types: list[SearchType]):
+            assert query == exp_query
+            assert limit == exp_limit
+            assert types == exp_types
+
+            nonlocal called
+            called = True
+
+    monkeypatch.setattr(ikea_api, "Search", MockSearch)
+    core.search(exp_query, exp_limit, exp_types)
+    assert called
