f03bcb9d3569072ea06e0189683435420bebd524
Lev Vereshchagin <mail@vrslev.com>
2021-12-28 12:25:41 +0300

    Move purchases test data to json files

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,3 +13,9 @@ def get_all_data_files_in_directory(dirname: str):
         with open(path) as f:
             res.append(json.load(f))
     return res
+
+
+def get_data_file(filename: str):
+    path = Path(__file__).parent / "data" / filename
+    with open(path) as f:
+        return json.load(f)
