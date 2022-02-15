3ab04e2cb72c0e9d45e2da421285d9d1bf19a67a
Lev Vereshchagin <mail@vrslev.com>
2022-02-15 13:07:29 +0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from new.constants import Constants
 from new.core import run
-from new.endpoints import item_pip
+from new.endpoints import pip_item
 
-api = item_pip.API(Constants())
+api = pip_item.API(Constants())
 res = run(api.get_item("89128566"))
 print(res)
