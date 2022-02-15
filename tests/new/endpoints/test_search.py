c851b1d31093bbf32bd7107e13fc4eb581ef5a0c
Lev Vereshchagin <mail@vrslev.com>
2022-02-15 15:33:56 +0300

    Add search tests

---
+++ tests/new/endpoints/test_search.py
@@ -0,0 +1,21 @@
+from new.constants import Constants
+from new.endpoints import search
+from tests.new.conftest import EndpointTester
+
+
+def test_search(constants: Constants):
+    query = "myquery"
+    limit = 30
+    types: list[search.SearchType] = ["PLANNER", "REFINED_SEARCHES"]
+
+    t = EndpointTester(search.API(constants).search(query, limit=limit, types=types))
+    req = t.prepare()
+
+    assert req.method == "GET"
+    assert req.url == ""
+    assert req.params
+    assert req.params["types"] == ",".join(types)
+    assert req.params["q"] == query
+    assert req.params["size"] == limit
+
+    t.assert_json_returned()
