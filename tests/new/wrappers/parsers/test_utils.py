2c1a1e3f13589756ef686f56e5de147060c910c9
Lev Vereshchagin <mail@vrslev.com>
2022-02-15 18:09:42 +0300

    update

--- tests/new/wrappers/parsers/test_utils.py
+++ tests/new/wrappers/parsers/test_utils.py
@@ -2,17 +2,17 @@ from new.constants import Constants
 from new.wrappers.parsers.utils import translate_from_dict
 
 
-def test_translate_no_value(constants: Constants):
-    constants.language = "nolang"
-    assert translate_from_dict({}, "v") == "v"
-    assert translate_from_dict({"en": {}}, "v") == "v"
-    constants.language = "ru"
+def test_translate_no_value():
+    constants = Constants(language="nolang")
+    assert translate_from_dict(constants, {}, "v") == "v"
+    assert translate_from_dict(constants, {"en": {}}, "v") == "v"
 
 
 def test_translate_with_value():
-    Constants.LANGUAGE_CODE = "nolang"
+    constants = Constants(language="nolang")
     assert (
-        translate_from_dict({"en": {"v": "en v"}, "nolang": {"v": "nolang v"}}, "v")
+        translate_from_dict(
+            constants, {"en": {"v": "en v"}, "nolang": {"v": "nolang v"}}, "v"
+        )
         == "nolang v"
     )
-    Constants.LANGUAGE_CODE = "ru"
